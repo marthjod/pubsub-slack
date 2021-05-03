6525b18a0d5a106d04d813878f466f6f4854e284
marthjod@gmail.com <marthjod@gmail.com>
2021-05-03 16:15:54 +0200

    run-develop.sh: fail faster

--- run-develop.sh
+++ run-develop.sh
@@ -5,25 +5,28 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
-go build -o cmd
 
 slack_token="${SLACK_TOKEN:-}"
+slack_channel="${SLACK_CHANNEL:-#chatops-dev}"
 gcp_project="${GCP_PROJECT:-}"
+pubsub_subscription="${PUBSUB_SUBSCRIPTION:-cluster-upgrades-dev}"
 
 if [ -z "${slack_token}" ]; then echo "need SLACK_TOKEN"; exit 1; fi
 if [ -z "${gcp_project}" ]; then echo "need GCP_PROJECT"; exit 1; fi
+# needs Pub/Sub Subscriber role
 if [ ! -f "service-account.json" ]; then echo "need service-account.json"; exit 1; fi
 
-export GCP_PROJECT="" # add GCP project here
-export SLACK_TOKEN="" # add Slack token here
-export PUBSUB_SUBSCRIPTION="slack-chatops-notifier"
-export LOGLEVEL="debug"
+go build -o cmd
 
+export GCP_PROJECT="${gcp_project}"
+export SLACK_TOKEN="${slack_token}"
+export PUBSUB_SUBSCRIPTION="${pubsub_subscription}"
+export SLACK_CHANNEL="${slack_channel}"
+export LOGLEVEL="debug"
 export GOOGLE_APPLICATION_CREDENTIALS="./service-account.json"
-export SLACK_CHANNEL="chatops-dev"
 
 ./cmd | jq -R 'fromjson? | .'
 
-# gcloud pubsub topics publish projects/my-project/topics/slack-chatops-notifications \
+# gcloud pubsub topics publish projects/${gcp_project}/topics/slack-chatops-notifications \
 #   --message "<@slackhandle> something has happened" \
 #   --attribute publish_time="$(date +%s)"
